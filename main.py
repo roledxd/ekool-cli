93c68915714718dddd92d473755f5cba7adaebc2
RoLedXD <44301044+roledxd@users.noreply.github.com>
2021-01-18 13:02:24 +0200

    Sign-in trycatch + username in prompt (name1 name2> )

--- main.py
+++ main.py
@@ -8,27 +8,27 @@
 # * Sisestades 'kodutoo' või 'k' saab kätte kõik praeguse nädala kodutööd.
 # 
 # 
-# eKool> voog 
-# eKool> v 
+# name1 name2> voog 
+# name1 name2> v 
 # Sisestades selle saab ekraanil näha kõiki hindeid ja teated. Igal hindel/teatel on oma ID, mille järgi saab
 # seda lähemalt vaadata.
 # 
-# eKool> voog [ID]
-# eKool> v [ID]
+# name1 name2> voog [ID]
+# name1 name2> v [ID]
 # Et uurida näiteks teate, mille ID'ks on 1 sisu, peame sisestame programmi 'voog 1' või lühendi 'v 1'.
 # See näitab meile lisainfot teate kohta. Sama nagu eKoolis teate peale vajutamine.
 # 
-# eKool> kodutoo
+# name1 name2> kodutoo
 # Sisestades 'kodutoo' saame näha kõiki praeguse nädala kodutöid.
 # 
-# eKool> kodutoo [esmaspaev, teisipaev, kolmapaev, neljapaev, reede]
-# eKool> k [e, t, k, n, r]
+# name1 name2> kodutoo [esmaspaev, teisipaev, kolmapaev, neljapaev, reede]
+# name1 name2> k [e, t, k, n, r]
 # Kui soovime näha näiteks kõiki esmaspäeva kodutöid, peame programmi sisestama kas 'kodutoo esmaspaev' või lühendi
 # 'k e', milles k='kodutoo' ja e='esmaspaev'
 # See toob meile koik esmaspaeva kodutood.
 #
-# eKool> kodutoo [esmaspaev, teisipaev, kolmapaev, neljapaev, reede] [ID]
-# eKool> k [e, t, k, n, r] [ID]
+# name1 name2> kodutoo [esmaspaev, teisipaev, kolmapaev, neljapaev, reede] [ID]
+# name1 name2> k [e, t, k, n, r] [ID]
 # Sisestame päeva, mille kodutööd tahame ning selle päeval valitud kodutöö ID.
 # Tagasi saame kogu kodutöö sisu koos manustega(kui need on olemas)
 
@@ -55,16 +55,20 @@ warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
 # Command-Line sarnane Class, mis küsib kogu aeg commande ja siis teeb midagi nende põhjal.
 class EKoolLoop(Cmd):
-    # Prompt, mida näidatakse iga commandi ees. 
-    prompt = 'eKool> '
 
     # Konstruktor
     def __init__(self):
         # Kui tahame oma konstruktorit kasutada, peame ennem callima ka Cmd classi konstruktorit.
         super(EKoolLoop, self).__init__()
-
-        # Loome eKooliga rääkiva classi instance. Selle kaudu saame võtta näiteks tunniplaani jne.
-        self.ekool = EKoolParser(EMAIL, PASSWORD)
+        
+        try:
+            # Loome eKooliga rääkiva classi instance. Selle kaudu saame võtta näiteks tunniplaani jne.
+            self.ekool = EKoolParser(EMAIL, PASSWORD)
+            # Prompt, mida näidatakse iga commandi ees. 
+            self.prompt = self.ekool.person_info['name1']+" "+self.ekool.person_info['name2']+"> "
+        except:
+            print("Unable to sign in, closing...")
+            exit(0)
 
 
     # Command -> 'eKool> v'
